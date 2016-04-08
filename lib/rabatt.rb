5f9295f7ec788e8df65a5e874125a8da6da7e6dd
Gustav <gustav@invoke.se>
2016-04-08 13:10:37 +0200

    Add Adrecord

--- lib/rabatt.rb
+++ lib/rabatt.rb
@@ -2,6 +2,7 @@ require "rabatt/version"
 
 require 'rabatt/providers/base'
 require 'rabatt/providers/adtraction'
+require 'rabatt/providers/adrecord'
 
 module Rabatt
   # Your code goes here...
---
+++ lib/rabatt/providers/adrecord.rb
@@ -0,0 +1,29 @@
+require 'open-uri'
+require 'saxerator'
+
+module Rabatt
+  module Providers
+    class Adrecord
+
+      ENDPOINT = 'https://www.adrecord.com/api/discountCodes.xml?c=%d'
+
+      def initialize
+      end
+
+      def coupons(channel_id)
+        res = open(ENDPOINT % channel_id)
+        parser = Saxerator.parser(res.read)
+        parser.for_tag(:discountcode).map do |item|
+          Voucher.new.tap do |v|
+            v.title = item['program']
+            v.summary = item['descriptions']
+            v.code = item['code']
+            v.expires_at = item['to']
+            v.valid_from = item['from']
+          end
+        end
+      end
+
+    end
+  end
+end
