81e24df8a8cea21afcdf22d717889721812d1ded
Gustav <gustav@invoke.se>
2016-04-08 13:10:59 +0200

    Add shortcut method to get new instance of provider class

--- lib/rabatt.rb
+++ lib/rabatt.rb
@@ -6,4 +6,7 @@ require 'rabatt/providers/adrecord'
 
 module Rabatt
   # Your code goes here...
+def Rabatt(provider_name, options = {})
+  class_name = provider_name.to_s.capitalize
+  Object.const_get("Rabatt::Providers::#{class_name}").new
 end
