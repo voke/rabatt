8cdcaf8a8539b0cb7ca43de0d5037d159abdd832
Gustav <gustav@invoke.se>
2016-04-08 18:08:06 +0200

    Define public methods in base class

--- lib/rabatt/providers/base.rb
+++ lib/rabatt/providers/base.rb
@@ -2,6 +2,14 @@ module Rabatt
   module Providers
     class Base
 
+      def vouchers_by_channel(*args)
+        raise NotImplementedError
+      end
+
+      def vouchers
+        raise NotImplementedError
+      end
+
     end
   end
 end
