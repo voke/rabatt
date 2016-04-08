cc98ecb36535a3739b5f73827aeee836947959e7
Gustav <gustav@invoke.se>
2016-04-08 18:03:56 +0200

    Handle empty timestamps for adrecord

--- lib/rabatt/providers/adrecord.rb
+++ lib/rabatt/providers/adrecord.rb
@@ -12,14 +12,19 @@ module Rabatt
 
       def coupons(channel_id)
         res = open(ENDPOINT % channel_id)
+      def parse_date(value)
+        if !value.is_a?(Saxerator::Builder::EmptyElement)
+          Date.strptime(value, DATE_FORMAT)
+        end
+      end
         parser = Saxerator.parser(res.read)
         parser.for_tag(:discountcode).map do |item|
           Voucher.build do |v|
             v.program = item['program']
             v.summary = item['description']
             v.code = item['code']
-            v.expires_at = Date.parse(item['to'])
-            v.valid_from = Date.parse(item['from'])
+            v.expires_at = parse_date(item['to'])
+            v.valid_from = parse_date(item['from'])
             v.url = item['url']
           end
         end
