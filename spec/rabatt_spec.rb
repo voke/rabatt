d16a56c266e9a2b224364e4e8df5fcff13714543
Gustav <gustav@invoke.se>
2016-04-08 16:16:46 +0200

    Add gems for testing

--- spec/rabatt_spec.rb
+++ spec/rabatt_spec.rb
@@ -1,11 +1,1 @@
 require 'spec_helper'
-
-class RabattTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Rabatt::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
-  end
-end
