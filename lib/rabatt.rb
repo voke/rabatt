c95da588f73591f913f64b1ac3ebed7d171dad5c
Gustav <gustav@invoke.se>
2016-04-08 13:11:16 +0200

    Add RequestError class

--- lib/rabatt.rb
+++ lib/rabatt.rb
@@ -5,7 +5,9 @@ require 'rabatt/providers/adtraction'
 require 'rabatt/providers/adrecord'
 
 module Rabatt
-  # Your code goes here...
+  RequestError = Class.new(StandardError)
+end
+
 def Rabatt(provider_name, options = {})
   class_name = provider_name.to_s.capitalize
   Object.const_get("Rabatt::Providers::#{class_name}").new
